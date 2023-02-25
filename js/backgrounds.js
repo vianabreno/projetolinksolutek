d0604d30516dce2777654fb4a7e116bd79416688
vianabreno <brenoviana365@gmail.com>
2023-02-25 18:42:20 -0300

    b
    b

---
+++ js/backgrounds.js
@@ -0,0 +1,6 @@
+document.body.style.backgroundImage = "url('imagens/gif01.gif')"
+document.body.style.backgroundSize = "cover"
+document.body.style.backgroundRepeat = "no-repeat"
+
+let perfil = document.querySelector('#perfil')
+perfil.style.backgroundImage = "url('imagens/ssolutek.png')"
